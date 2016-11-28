b9a16edd4cb6401788e8857dad764eda2169b809
mrnehemiahreese <nehemiahreese@gmail.com>
2016-11-27 23:11:25 -0600

    Add files via upload

---
+++ PlayerManager.java
@@ -0,0 +1,33 @@
+
+public class PlayerManager {
+
+	
+	
+
+	 public static void main(String[] args) {
+		 CreatePlayers();
+
+	 }
+	
+	
+	
+	 
+	 
+	 
+	 public static int CreatePlayers() {
+	        // Makes Array of team names
+		 String[] playerNames = {"Jared Goff ", "	Carson Wentz ", 
+			        "Joey Bosa ", "DeMarco Murray", "Peyton Manning", "Calvin Johnson",
+			        "Andrew Luck", "Antonio Brown", "Jack Conklin ", "Rob Gronkowski",
+			        "Richard Sherman", "Ryan Kelly ", "Julio Jones", "Luke Kuechly", 
+			        "Dez Bryant", "Le'Veon Bell", "Darrelle Revis", "Jordy Nelson",
+			        "Patrick Peterson", "Demaryius Thomas", "Earl Thomas", 
+			        "Russell Wilson", "Joe Haden", "Joe Thomas", "Justin Houston",
+			        "Gerald McCoy", "LeSean McCoy", "Drew Brees", "Jimmy Graham", 
+			        "Von Miller", "Tony Romo", "Tyron Smith"};
+	        return 0;
+	    }
+
+	 
+	
+}
